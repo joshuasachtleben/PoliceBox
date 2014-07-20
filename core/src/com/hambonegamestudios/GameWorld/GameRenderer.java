aa09faadd214d890a141e1b0f53f63053322e251
Josh <joshuasachtleben@gmail.com>
2014-07-19 20:43:17 -0400

    Added rectangle shape in GameWorld and rendered in GameRenderer.

--- core/src/com/hambonegamestudios/GameWorld/GameRenderer.java
+++ core/src/com/hambonegamestudios/GameWorld/GameRenderer.java
@@ -1,9 +1,10 @@
 package com.hambonegamestudios.GameWorld;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.hambonegamestudios.PoliceBox.PBGame;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
 /**
  * Created by: Joshua Sachtleben
@@ -34,5 +35,42 @@ public class GameRenderer {
     public void render() {
         System.out.println("GameRenderer - render() called");
 
+        /*
+            1. Draw a black background to prevent flickering.
+         */
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        /*
+            2. Draw the filled rectangle
+         */
+
+        // Tells shapeRenderer to begin drawing filled shapes
+        shapeRenderer.begin(ShapeType.Filled);
+
+        // Choose an RGB color with full opacity
+        shapeRenderer.setColor(16/255.0f, 35/255.0f, 114/255.0f, 1.0f);
+
+        // Draw the rectangle from myWorld
+        shapeRenderer.rect(myWorld.getRectangle().x, myWorld.getRectangle().y, myWorld.getRectangle().width, myWorld.getRectangle().height);
+
+        // Tells shapeRenderer to finish rendering
+        shapeRenderer.end();
+
+        /*
+            3. Draw the rectangle's outline
+         */
+
+        // Tells shapeRenderer to begin drawing the outline shape
+        shapeRenderer.begin(ShapeType.Line);
+
+        // Choose an RGB color with full opacity
+        shapeRenderer.setColor(255/255.0f, 255/255.0f, 255/255.0f, 1.0f);
+
+        // Draw the rectangle
+        shapeRenderer.rect(myWorld.getRectangle().x, myWorld.getRectangle().y, myWorld.getRectangle().width, myWorld.getRectangle().height);
+
+        // Tells shapeRenderer to finish rendering
+        shapeRenderer.end();
     }
 }
--- core/src/com/hambonegamestudios/GameWorld/GameWorld.java
+++ core/src/com/hambonegamestudios/GameWorld/GameWorld.java
@@ -1,5 +1,8 @@
 package com.hambonegamestudios.GameWorld;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Rectangle;
+
 /**
  * Created by: Joshua Sachtleben
  * Date:       7/19/2014
@@ -7,7 +10,29 @@ package com.hambonegamestudios.GameWorld;
  * Project:    PoliceBox
  */
 public class GameWorld {
+
+    private Rectangle rectangle = new Rectangle(0, 0, 25, 50);
+    private int width = Gdx.graphics.getWidth();
+    private int height = Gdx.graphics.getHeight();
+    private int xVel = 1, yVel = 1;
+
     public void update(float delta) {
         System.out.println("GameWorld - update() called");
+        if(rectangle.x + rectangle.getWidth() > width) {
+            xVel = -3;
+        } else if (rectangle.x < 0) {
+            xVel = 3;
+        } else if (rectangle.y + rectangle.getHeight() > height) {
+            yVel = -3;
+        } else if (rectangle.y < 0) {
+            yVel = 3;
+        }
+
+        rectangle.x += xVel;
+        rectangle.y += yVel;
+    }
+
+    public Rectangle getRectangle() {
+        return rectangle;
     }
 }
