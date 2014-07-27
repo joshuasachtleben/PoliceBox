250fc8ceeaaa29d7e267b813514628f0e1820e97
Josh <joshuasachtleben@gmail.com>
2014-07-26 20:37:21 -0400

    Set background to only render in camera viewport.

--- core/src/com/hambonegamestudios/GameWorld/GameRenderer.java
+++ core/src/com/hambonegamestudios/GameWorld/GameRenderer.java
@@ -21,19 +21,26 @@ public class GameRenderer {
 
     private GameWorld myWorld;
     private OrthographicCamera camera;
-    private int width;
-    private int height;
+    private int width, height;
+    private int tilesWidth, tilesHeight;
+    private int cameraWidth, cameraHeight;
     private TARDIS tardis;
     private SpriteBatch batch;
+    private float cameraZoom;
 
     public GameRenderer(GameWorld world) {
         myWorld = world;
         width = Gdx.graphics.getWidth();
         height = Gdx.graphics.getHeight();
         camera = new OrthographicCamera();
+        cameraWidth = width;
+        cameraHeight = height;
+        cameraZoom = 1.0f;
         // may be able to divide width and height by 2 (or more) to scale objects when drawn
-        camera.setToOrtho(true, width/3, height/3); // TODO Find a way to do this better.  Match the denominator found in TARDIS.java
+        camera.setToOrtho(true, 100, 100); // TODO Find a way to do this better.  Match the denominator found in TARDIS.java
         System.out.println("Orthographic Camera created with dimensions " + width + " x " + height);
+
+
 
         batch = new SpriteBatch();
         batch.setProjectionMatrix(camera.combined);
@@ -48,19 +55,48 @@ public class GameRenderer {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        // Set camera properties
         camera.position.set(tardis.getPosition().x + tardis.getWidth()/2, tardis.getPosition().y + tardis.getHeight()/2, 0);
+        camera.viewportWidth = cameraWidth;
+        camera.viewportHeight = cameraHeight;
+        camera.zoom = cameraZoom;
         camera.update();
         batch.setProjectionMatrix(camera.combined);
 
         // Draw the animated sprite
         batch.begin();
 
-        for (int y = 0; y < myWorld.getHeight() / AssetLoader.backgroundTexture.getHeight(); y++) {
-            for (int x = 0; x < myWorld.getWidth() / AssetLoader.backgroundTexture.getWidth(); x++) {
-                //System.out.println("Tiles: " + x);
-                batch.draw(AssetLoader.backgroundTexture, x * AssetLoader.backgroundTexture.getWidth(), y * AssetLoader.backgroundTexture.getHeight());
-            }
-        }
+//        for (int y = -2; y < myWorld.getHeight() / AssetLoader.backgroundTexture.getHeight() + 2; y++) {
+//            for (int x = 0; x < myWorld.getWidth() / AssetLoader.backgroundTexture.getWidth(); x++) {
+//                //System.out.println("Tiles: " + x);
+//                batch.draw(AssetLoader.backgroundTexture, x * AssetLoader.backgroundTexture.getWidth(), y * AssetLoader.backgroundTexture.getHeight());
+//            }
+//        }
+
+        /*
+            http://www.java-gaming.org/index.php?topic=32157.0
+            Rendering the background.  batch.draw takes the following arguments:
+            texture: this is the texture that is created in AssetLoader.
+            x: Where to start drawing on the x-axis.  We have this as the negative value of half the width of the current camera view.  This will limit the drawing of repeated textures to the left most side of the camera viewing area
+            y: Where to start drawing on the y-axis.  We have this as the negative value of half the height of the current camera view.  This will limit the drawing of repeated textures to the top most side of the camera viewing area
+            width: How far across to draw the rectangle that is displaying the texture
+            height: How far from top to bottom to draw the rectangle that is displaying the texture
+            u: starting x-coordinate of texture (left)
+            v: starting y-coordinate of texture (bottom)
+            u2: ending x-coordinate of texture (right)
+            v2: ending y-coordinate of texture (top)
+            Note: since this batch.draw() method is using a texture that is set to repeat itself when it reaches the end of the drawing area, this method will allow repeating for the full width and height specified.
+                  For example, if you had a texture that was 512x512, and you specified a u,v of (0, 900) and a u2,v2 of (720, 0), and if you didn't have the texture repeat, it would display the 512x512 texture at the top, left, and then just black (or whatever was behind) at the bottom, right.
+
+         */
+        tilesWidth = width + cameraWidth / AssetLoader.backgroundTexture.getWidth();
+        tilesHeight = height + cameraHeight / AssetLoader.backgroundTexture.getHeight();
+        batch.draw(AssetLoader.backgroundTexture, -cameraWidth / 2, -cameraHeight / 2,
+                AssetLoader.backgroundTexture.getWidth() * tilesWidth,
+                AssetLoader.backgroundTexture.getHeight() * tilesHeight,
+                0, tilesHeight,
+                tilesWidth, 0
+                );
 
         //AssetLoader.stars.draw(batch);
 
@@ -71,5 +107,13 @@ public class GameRenderer {
 
     public void initGameObjects() {
         tardis = myWorld.getTardis();
+    }
+
+    public float getCameraZoom () {
+        return cameraZoom;
+    }
+
+    public void setCameraZoom (int zoomAmount) {
+        cameraZoom += zoomAmount;
     }
 }
